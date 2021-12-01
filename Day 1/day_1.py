073595b18557772f873d0de652f00601924026fa
watsonka99 <watsonkieran1@gmail.com>
2021-12-01 05:37:23 +0000

    Day 1 challenge complete

---
+++ Day 1/day_1.py
@@ -0,0 +1,29 @@
+def file_to_int_arr(input:str)->list[int]:
+    arr = []
+    with open(input, "r") as f:
+        for line in f:
+            arr.append(int(line))
+    return arr
+
+test_input = file_to_int_arr("Day 1/test.txt")
+input = file_to_int_arr("Day 1/input.txt")        
+
+def part_1(input: list[int])-> int:
+    increase = 0
+    for idx in range(1, len(input)):
+        if input[idx] > input[idx-1]:
+            increase += 1
+    return increase
+
+print(part_1(test_input))
+print(part_1(input))
+
+def part_2(input: list[int])-> int:
+    increase = 0
+    for idx in range(3, len(input)):
+        if input[idx] > input[idx-3]:
+            increase += 1
+    return increase
+
+print(part_2(test_input))
+print(part_2(input))
